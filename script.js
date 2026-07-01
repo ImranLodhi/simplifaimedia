562aa5862b3ad683f561b98c218b7af2554ac31b
ImranLodhi <reachimranlodhi@gmail.com>
2026-06-30 21:40:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const glow = document.querySelector(".cursor-glow");
 
-document.addEventListener("mousemove",(e)=>{
-
-    glow.style.left=e.clientX+"px";
-
-    glow.style.top=e.clientY+"px";
-
-});
